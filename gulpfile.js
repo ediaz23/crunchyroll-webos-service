0581a18252fa69ef22f1e2fe5bd0619b424acb1f
ediaz23 <esteban.diazt23@gmail.com>
2024-10-18 15:48:48 -0300

    [IMP] cancel function, better log and conditional compiling

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,22 @@ const gulp = require('gulp')
 const jshint = require('gulp-jshint')
 const del = require('del')
 const exec = require('child_process').exec
+const { Transform } = require('stream');
+const conditionalLoader = require('webpack-conditional-loader')
 
+
+function conditionalCompiler() {
+    return new Transform({
+        objectMode: true,
+        transform(file, encoding, callback) {
+            if (file.isBuffer()) {
+                const content = file.contents.toString(encoding);
+                file.contents = Buffer.from(conditionalLoader(content));
+            }
+            callback(null, file);
+        }
+    });
+}
 
 gulp.task('clean', function() {
     return del('dist/**', { force: true });
@@ -18,13 +33,7 @@ gulp.task('index', () =>
     gulp.src('src/index.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-        .pipe(gulp.dest('dist/src'))
-)
-
-gulp.task('logger', () =>
-    gulp.src('src/logger.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
+        .pipe(conditionalCompiler())
         .pipe(gulp.dest('dist/src'))
 )
 
@@ -43,7 +52,7 @@ function nodeInstall(cb, extra) {
 gulp.task('node-insta', (cb) => { nodeInstall(cb, '') })
 gulp.task('node-insta-p', (cb) => { nodeInstall(cb, '--omit=dev') })
 
-gulp.task('build', gulp.series('clean', 'misc', 'index', 'logger', 'node-insta'));
-gulp.task('build-p', gulp.series('clean', 'misc', 'index', 'logger', 'node-insta-p'));
+gulp.task('build', gulp.series('clean', 'misc', 'index', 'node-insta-p'));
+gulp.task('build-p', gulp.series('clean', 'misc', 'index', 'node-insta-p'));
 
 module.exports = gulp
